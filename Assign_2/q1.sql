f29a1d56fc24b43856ad1279660275436f692cc9
Shawn Li <hydronium0712@gmail.com>
2019-11-01 15:51:02 -0400

    Adding q1.sql

--- Assign_2/q1.sql
+++ Assign_2/q1.sql
@@ -3,4 +3,4 @@ FROM Person p1, Passenger p2
 WHERE  (YEAR (p1.DateOfBirth)) <= 2001 AND p1.ID = p2.PassengerID AND p1.Occupation LIKE '%Student' AND p1.ID IN 
                     (SELECT t1.PassengerID
                     FROM Take t1
-                    WHERE t1.FlightNumber = 11 AND (DAY (t1.Date)) = 20 AND((MONTH (t1.Date)) = 'January'));
+                    WHERE t1.FlightNumber = 11 AND (DAY (t1.Date)) = 20 AND((MONTH (t1.Date)) = 1));
